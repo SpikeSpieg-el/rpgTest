994ab8889addf9acc0457b5fd2f073692e8cf099
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2024-08-30 14:33:57 +0500

    1

--- js/main.js
+++ js/main.js
@@ -5,19 +5,25 @@
 PluginManager.setup($plugins);
 
 window.onload = function() {
-    // Инициализация vkBridge
-    vkBridge.send('VKWebAppInit');
+    // Инициализация VK Bridge
+    vkBridge.send("VKWebAppInit", {})
+        .then(() => {
+            console.log('VK Bridge инициализирован');
 
-    // Подключение к событиям VK Bridge
-    vkBridge.subscribe((event) => {
-        console.log(event);
-        if (event.detail.type === 'VKWebAppUpdateConfig') {
-            const schemeAttribute = document.createAttribute('scheme');
-            schemeAttribute.value = event.detail.data.scheme ? event.detail.data.scheme : 'client_light';
-            document.body.attributes.setNamedItem(schemeAttribute);
-        }
-    });
+            // Подключение к событиям VK Bridge
+            vkBridge.subscribe((event) => {
+                console.log(event);
+                if (event.detail.type === 'VKWebAppUpdateConfig') {
+                    const schemeAttribute = document.createAttribute('scheme');
+                    schemeAttribute.value = event.detail.data.scheme ? event.detail.data.scheme : 'client_light';
+                    document.body.attributes.setNamedItem(schemeAttribute);
+                }
+            });
 
-    // Инициализация игры
-    SceneManager.run(Scene_Boot);
+            // Инициализация игры
+            SceneManager.run(Scene_Boot);
+        })
+        .catch((error) => {
+            console.error('Ошибка при инициализации VK Bridge:', error);
+        });
 };
