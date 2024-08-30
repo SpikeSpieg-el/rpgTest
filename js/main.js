c7b0bcef4fec022c4f1719bc8ce56a7888251af7
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2024-08-30 14:23:48 +0500

    проба на вк

--- js/main.js
+++ js/main.js
@@ -5,5 +5,19 @@
 PluginManager.setup($plugins);
 
 window.onload = function() {
+    // Подключение VK Bridge
+    vkBridge.send('VKWebAppInit');
+
+    // Подключение к событиям VK Bridge
+    vkBridge.subscribe((event) => {
+        console.log(event);
+        if (event.detail.type === 'VKWebAppUpdateConfig') {
+            const schemeAttribute = document.createAttribute('scheme');
+            schemeAttribute.value = event.detail.data.scheme ? event.detail.data.scheme : 'client_light';
+            document.body.attributes.setNamedItem(schemeAttribute);
+        }
+    });
+
+    // Инициализация игры
     SceneManager.run(Scene_Boot);
 };
